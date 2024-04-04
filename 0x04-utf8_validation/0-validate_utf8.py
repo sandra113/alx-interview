af853c0ae2f28f9e6dbcd53bdb77bcaafebe35a9
sandra113 <sandramabonga303@gmail.com>
2024-04-04 12:55:20 -0700

    README.md

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,40 @@
 #!/usr/bin/python3
+""" UTF-8 Validation """
 
 
 def validUTF8(data):
-    # Variable to track the number of bytes expected for the next character
-    bytes_to_follow = 0
+    """
+    Method that determines if a given data set represents a valid
+    UTF-8 encoding.
+    """
+    number_bytes = 0
 
-    for byte in data:
-        # Check if this byte is a continuation byte (starts with '10')
-        if bytes_to_follow > 0:
-            if (byte >> 6) != 0b10:
+    mask_1 = 1 << 7
+    mask_2 = 1 << 6
+
+    for i in data:
+
+        mask_byte = 1 << 7
+
+        if number_bytes == 0:
+
+            while mask_byte & i:
+                number_bytes += 1
+                mask_byte = mask_byte >> 1
+
+            if number_bytes == 0:
+                continue
+
+            if number_bytes == 1 or number_bytes > 4:
                 return False
-            bytes_to_follow -= 1
+
         else:
-            # Determine the number of bytes expected for the next character
-            if byte >> 7 == 0:
-                bytes_to_follow = 0  # 1-byte character, no continuation bytes
-            elif byte >> 5 == 0b110:
-                bytes_to_follow = 1  # 2-byte character, 1 continuation byte
-            elif byte >> 4 == 0b1110:
-                bytes_to_follow = 2  # 3-byte character, 2 continuation bytes
-            elif byte >> 3 == 0b11110:
-                bytes_to_follow = 3  # 4-byte character, 3 continuation bytes
-            else:
-                return False  # Invalid UTF-8 prefix
+            if not (i & mask_1 and not (i & mask_2)):
+                    return False
+
+        number_bytes -= 1
+
+    if number_bytes == 0:
+        return True
+
+    return False

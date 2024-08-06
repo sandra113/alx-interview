4713d6eaaeb50915a12882d7bdcebc21737f0c61
sandra113 <sandramabonga303@gmail.com>
2024-08-06 12:45:10 +0300

    nqueens

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,80 +1,44 @@
 #!/usr/bin/python3
-"""this module attempts to solve the nqueens problem"""
-
+""" N queens """
 import sys
 
 
-class Nqueens:
-    """a class that has the method and util
-        functions to solve the nqueens problem"""
+if len(sys.argv) > 2 or len(sys.argv) < 2:
+    print("Usage: nqueens N")
+    exit(1)
 
-    def __init__(self, nums):
-        """the entry point into the class instance"""
-        self.nums = nums
-        self.board = []
+if not sys.argv[1].isdigit():
+    print("N must be a number")
+    exit(1)
 
-    def solve(self):
-        """this utility performs validation on the command line argument N
-            before attempting to solve the N-queens problem"""
-        self.cols = set()
-        self.rows = set()
-        self.pos_diag = set()
-        self.neg_diag = set()
-        self.res = []
-        self.hash = dict()
+if int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    exit(1)
 
-        if (type(self.nums) is not int):
-            try:
-                self.nums = int(self.nums)
-            except Exception:
-                print("N must be a number")
-                return
-        if (self.nums < 4):
-            print("N must be at least 4")
-            return
-        self.backtrack(0)
-        return self.res
-
-    def backtrack(self, r):
-        """the main backtracking algorithm used in the n-queens solution"""
-        if (r == self.nums):
-            self.res.append(list(self.hash.values()))
-            self.hash = dict()
-            return
-
-        for c in range(self.nums):
-            if (c in self.cols or (r - c) in self.neg_diag or
-                    (r + c) in self.pos_diag):
-                continue
-            self.cols.add(c)
-            self.neg_diag.add(r - c)
-            self.pos_diag.add(r + c)
-            self.hash[f"{r}{c}"] = [r, c]
-            self.board.append(self.hash[f"{r}{c}"])
-
-            self.backtrack(r + 1)
-
-            self.cols.remove(c)
-            self.neg_diag.remove(r - c)
-            self.pos_diag.remove(r + c)
-            if (f"{r}{c}" in self.hash):
-                self.hash.pop(f"{r}{c}")
+n = int(sys.argv[1])
 
 
-def main():
-    """the entry point into the program"""
-    if __name__ != "__main__":
-        return
-    if (len(sys.argv) < 2):
-        print("no arguments provided!")
-        return
-    input = sys.argv[1]
-    queens = Nqueens(input)
-    solution = queens.solve()
-    if (not solution):
-        return
-    for i in solution:
-        print(i)
+def queens(n, i=0, a=[], b=[], c=[]):
+    """ find possible positions """
+    if i < n:
+        for j in range(n):
+            if j not in a and i + j not in b and i - j not in c:
+                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+    else:
+        yield a
 
 
-main()
+def solve(n):
+    """ solve """
+    k = []
+    i = 0
+    for solution in queens(n, 0):
+        for s in solution:
+            k.append([i, s])
+            i += 1
+        print(k)
+        k = []
+        i = 0
+
+
+solve(n)
